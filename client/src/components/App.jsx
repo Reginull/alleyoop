673c93c8bdc47ab12e522d8875574cca6b4f136e
Wesley Mak <wesleymak005@gmail.com>
2024-10-27 11:31:04 -0400

    Connected frontend & backend

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import NavBar from './navbar.jsx'
 import SideBar from './sidebar.jsx'
 import '../styles.css'
 
+
 function App() {
+
+    // eslint-disable-next-line no-unused-vars
+    const [backendData, setBackendData] = useState(0);
+
+    const fetchAPI = async () => {
+        const response = await axios.get("http://localhost:3000/api")
+        console.log(response.data)
+    }
+  
+    useEffect(() => {
+        fetchAPI();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return(
         <div className='App'>
             <NavBar> </NavBar>
